b1208115b33f4ba3251e022ef0368e9abfcaf23e
Marina Merzlyakova <marina.merzlyakova1009@yandex.ru>
2023-11-06 23:13:54 +0300

    Удаляет проект

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ Component, loggedIn, ...props }) => {
-  
-  return loggedIn ? (<Component {...props} />) : (<Navigate to="/" replace />);
-}
-
-export default ProtectedRoute;
